5bfff6821cf919d9f93b3777ac0ccf29b3cc7583
Jabo Fecht <jifecht@gmail.com>
2023-05-04 11:30:56 +0200

    Update sizings on the circle and stylings, add button selectability

--- src/components/ProgressCircle.tsx
+++ src/components/ProgressCircle.tsx
@@ -1,38 +1,40 @@
-import { FC } from "react"
+import { FC } from "react";
 
-interface ProgressCircleProps {}
-
-const ProgressCircle: FC<ProgressCircleProps> = ({}) => {
-	const radius = 50
-	const stroke = 2
-	const normalizedRadius = radius - stroke * 2
-	const circumference = normalizedRadius * 2 * Math.PI
-	const progress = 99
-	const strokeDashoffset = circumference - (progress / 100) * circumference
-
-	return (
-		<svg
-			viewBox="0 0 100 100"
-			xmlns="http://www.w3.org/2000/svg"
-			className="absolute"
-		>
-			<circle
-				fill="transparent"
-				strokeWidth={stroke}
-				stroke="rgb(239, 68, 68)"
-				cx={radius}
-				cy={radius}
-				r={normalizedRadius}
-				strokeDasharray={circumference + " " + circumference}
-				style={{
-					strokeDashoffset,
-					transition: "stroke-dashoffset 1s",
-					transform: "rotate(-90deg)",
-					transformOrigin: "50% 50%",
-				}}
-			/>
-		</svg>
-	)
+interface ProgressCircleProps {
+  stroke: number;
+  progress: number;
+  strokeColor: string;
 }
 
-export default ProgressCircle
+const ProgressCircle: FC<ProgressCircleProps> = ({
+  stroke,
+  progress,
+  strokeColor,
+}) => {
+  const RADIUS = 50;
+  const normalizedRadius = RADIUS - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const strokeDashoffset = circumference - (progress / 100) * circumference;
+
+  return (
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className="">
+      <circle
+        fill="transparent"
+        strokeWidth={stroke}
+        stroke={strokeColor}
+        cx={RADIUS}
+        cy={RADIUS}
+        r={normalizedRadius}
+        strokeDasharray={circumference + " " + circumference}
+        style={{
+          strokeDashoffset,
+          transition: "stroke-dashoffset 1s",
+          transform: "rotate(-90deg)",
+          transformOrigin: "50% 50%",
+        }}
+      />
+    </svg>
+  );
+};
+
+export default ProgressCircle;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,90 @@
-import ProgressCircle from "@/components/ProgressCircle"
-import { SettingsIcon } from "@/components/SettingsIcon"
+import ProgressCircle from "@/components/ProgressCircle";
+import { SettingsIcon } from "@/components/SettingsIcon";
+import { useState } from "react";
+
+type buttonActive = 1 | 2 | 3;
 
 export default function Home() {
-	const color = "red"
-	const minutes = 20
-	const seconds = 59
+  const color = "red";
+  const [timeLeft, setTimeLeft] = useState<number>(20);
+  const [activeButton, setActiveButton] = useState<buttonActive>(2);
+  const accentColor = "#E06469";
+  const accentColorTailwind = "bg-[#E06469]";
 
-	return (
-		<div className="font-poppins font-bold h-screen bg-violet-950 flex justify-center items-center">
-			<div className="w-full lg:w-1/2 max-w-[700px] h-full flex flex-col items-center gap-10">
-				{/* Heading */}
-				<div className="w-full mt-10">
-					<h1 className="text-2xl md:text-3xl text-center text-neutral-300">
-						pomodoro
-					</h1>
-				</div>
-				{/* Buttons */}
-				<div className="w-4/5 h-14 rounded-full flex gap-2 bg-violet-900 text-gray-300">
-					<button
-						className={`w-1/3 h-full rounded-full ${
-							color === "red" ? "hover:bg-red-500" : null
-						} shadow-slate-500 drop-shadow-lg hover:shadow-inner hover:shadow-black`}
-					>
-						pomodoro
-					</button>
-					<button
-						className={`w-1/3 h-full rounded-full ${
-							color === "red" ? "hover:bg-red-500" : null
-						} shadow-slate-500 drop-shadow-lg hover:shadow-inner hover:shadow-black`}
-					>
-						short break
-					</button>
-					<button
-						className={`w-1/3 h-full rounded-full ${
-							color === "red" ? "hover:bg-red-500" : null
-						} shadow-slate-500 drop-shadow-lg hover:shadow-inner hover:shadow-black`}
-					>
-						long break
-					</button>
-				</div>
-				{/* Counter */}
-				<div className="relative w-full flex-col aspect-square bg-violet-900 rounded-full flex justify-center items-center">
-					<ProgressCircle />
-					<p className="text-[100px] text-neutral-300">
-						{minutes + ":" + seconds}
-					</p>
-					<button className="absolute tracking-widest bottom-1/4 text-4xl text-neutral-300">
-						PAUSE
-					</button>
-				</div>
-				{/* Settings */}
-				<div className="w-full flex items-center justify-center h-12">
-					<SettingsIcon />
-				</div>
-			</div>
-		</div>
-	)
+  const getLeftOverSeconds = (timeInMinutes: number): string => {
+    const roundedDownMinutes = Math.floor(timeInMinutes);
+    const leftOverSeconds = (timeInMinutes - roundedDownMinutes) * 60;
+    return leftOverSeconds.toString().padStart(2, "0");
+  };
+  return (
+    <div className="font-poppins font-bold h-screen bg-violet-950 flex justify-center items-center">
+      <div className="w-full lg:w-1/2 max-w-[700px] h-full flex flex-col items-center gap-10">
+        {/* Heading */}
+        <div className="w-full mt-10">
+          <h1 className="text-2xl md:text-3xl text-center text-neutral-300">
+            pomodoro
+          </h1>
+        </div>
+        {/* Buttons */}
+        <div className="text-sm w-3/5 h-14 rounded-full flex gap-2 bg-violet-900 text-gray-300">
+          <button
+            onClick={() => setActiveButton((prev) => 1)}
+            className={`w-1/3 h-full rounded-full ${
+              activeButton === 1 &&
+              `${accentColorTailwind} shadow-inner shadow-black`
+            } shadow-slate-500 drop-shadow-lg ${
+              activeButton !== 1 &&
+              "hover:shadow-inner hover:shadow-black hover:bg-gray-800"
+            } `}
+          >
+            pomodoro
+          </button>
+          <button
+            onClick={() => setActiveButton((prev) => 2)}
+            className={`w-1/3 h-full rounded-full ${
+              activeButton === 2 &&
+              `${accentColorTailwind} shadow-inner shadow-black`
+            } shadow-slate-500 drop-shadow-lg ${
+              activeButton !== 2 &&
+              "hover:shadow-inner hover:shadow-black hover:bg-gray-800"
+            } `}
+          >
+            short break
+          </button>
+          <button
+            onClick={() => setActiveButton((prev) => 3)}
+            className={`w-1/3 h-full rounded-full ${
+              activeButton === 3 &&
+              `${accentColorTailwind} shadow-inner shadow-black`
+            } shadow-slate-500 drop-shadow-lg ${
+              activeButton !== 3 &&
+              "hover:shadow-inner hover:shadow-black hover:bg-gray-800"
+            } `}
+          >
+            long break
+          </button>
+        </div>
+        {/* Counter */}
+        <div className="relative w-96 h-96 flex-col flex justify-center items-center">
+          <div className="w-full h-full rounded-full bg-violet-900">
+            <ProgressCircle
+              stroke={2}
+              progress={50}
+              strokeColor={accentColor}
+            />
+          </div>
+          <p className="absolute text-7xl text-neutral-300">
+            {timeLeft + ":" + getLeftOverSeconds(timeLeft)}
+          </p>
+          <button className="absolute bottom-1/4 text-xl text-neutral-300">
+            P A U S E
+          </button>
+        </div>
+        {/* Settings */}
+        <div className="w-full flex items-center justify-center h-12">
+          <SettingsIcon strokeColor={accentColor} />
+        </div>
+      </div>
+    </div>
+  );
 }
