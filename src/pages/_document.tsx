7b1b820d20457c2d04a0a5f54df1c16ea570daee
Jabo Fecht <jifecht@gmail.com>
2023-05-30 18:28:29 +0200

    Add title and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<title>Pomodoro App</title>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,700;1,800&display=swap"
 					rel="stylesheet"
 				/>
+				<link rel="shortcut icon" href="/favicon.png" />
 			</Head>
 			<body>
 				<Main />
